1b8c0459541fe140e50f6c99f4db9d91b5dbc74f
Raphalinho91 <lokiju1598@outlook.fr>
2024-02-20 12:03:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Companies from "./Components/Companies";
+import Details from "./Components/Details";
+import Footer from "./Components/Footer";
+import GetStarted from "./Components/GetStarted";
+import Guide from "./Components/Guide";
+import Hero from "./Components/Hero";
+import Properties from "./Components/Properties";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Companies />
+      <Guide />
+      <Properties />
+      <Details />
+      <GetStarted />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
